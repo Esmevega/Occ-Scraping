0630bf6f6aeb6c4fdcd43bcc66045b71ab5de118
EsmeVega <20191222@uthh.edu.mx>
2025-08-07 00:46:52 -0600

    Cambiando puerto de 3000 a 8080

--- occ-jobs-back/index.js
+++ occ-jobs-back/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const scrapeJobs = require("./scrapping");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
