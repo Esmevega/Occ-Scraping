9643eed5602796dfa670490a63a154b66fbde0f5
EsmeVega <20191222@uthh.edu.mx>
2025-08-08 22:37:20 -0600

    subiendo

--- occ-jobs-front/src/components/footer.jsx
+++ occ-jobs-front/src/components/footer.jsx
@@ -3,8 +3,16 @@ import { StyleSheet } from 'react-native';
 
 const footer = () => {
   return (
-    <footer style={Styles.Container} className="footer">
-      <div style={Styles.Text} className="container">
+    <footer style={{
+        backgroundColor: "#f8f9fa",
+        padding: "20px 0",
+        marginTop: "40px",
+        borderTop: "1px solid #e7e7e7",
+        textAlign: "center",}} 
+        className="footer">
+      <div style={{
+        padding: '0 32px'}} 
+        className="container">
         <p className="text-muted">Datos extraidos con fines educativos © 2025.</p>
       </div>
     </footer>
@@ -12,16 +20,3 @@ const footer = () => {
 }
 
 export default footer;
-
-const Styles = StyleSheet.create ({
-    Container: {
-        backgroundColor: "#f8f9fa",
-        padding: "20px 0",
-        marginTop: "40px",
-        borderTop: "1px solid #e7e7e7",
-        textAlign: "center",
-    },
-    Text: {
-        padding: '0 32px'
-    },
-})
